fd2bf0c2919075304d843dbd2318df1920445d5e
ahmad-bhai <nasiriq768@gmail.com>
2026-09-21 00:20:51 +0500

    Update PHOTO_URL to new image link

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const app = Express();
 app.use(Express.json());
 
 const BOT_TOKEN = process.env.BOT_TOKEN || "8828282862:AAE6gnK7g5e-IFyXaQ_OoskgcRYzoUJ7BIY";
-const PHOTO_URL = "https://i.ibb.co/bRJJJcCv/a155df819f25.jpg";
+const PHOTO_URL = "https://magic-scripts.vercel.app/ubs.png";
 
 // Helper function to send HTTP POST request to Telegram
 function callTelegramApi(method, payload) {
